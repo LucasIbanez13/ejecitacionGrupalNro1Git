1a1b89094c7fce0c871f94dc6f2c802b1a0366ae
LucasIbanez13 <lucaas00129@gmail.com>
2023-05-22 23:56:20 -0300

    Agrega receta en manzana.js

--- manzana.js
+++ manzana.js
@@ -1,1 +1,11 @@
 console.log("manzana actualizada");
+console.log(`tarta de manzana
+Ingrediente
+200g. de harina leudante
+12 cdas. de azúcar
+2 cdas. de coco rallado (opcional)*
+100g. de manteca
+250ml. de leche
+2 huevos
+3 manzanas
+1 puñado de almendras`)
